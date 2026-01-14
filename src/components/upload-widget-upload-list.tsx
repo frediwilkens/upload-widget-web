0e39ae211e3a58450baaaef2a00a2964cad682e8
Fredi Wilkens <frediwilkens@gmail.com>
2026-01-14 14:16:36 -0300

    add animations and circular-progress-bar component

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,6 +1,8 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList() {
+  const isUploadListEmpty = true;
+
   return (
     <div className="px-3 flex flex-col gap-3">
       <span className="text-xs font-medium">
@@ -8,11 +10,15 @@ export function UploadWidgetUploadList() {
         <span className="text-zinc-400">{2}</span>
       </span>
 
-      <div className="flex flex-col gap-2">
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-      </div>
+      {isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
     </div>
   )
 }
